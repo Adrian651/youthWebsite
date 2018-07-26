f32d686651bdfc267c6b2191c5d31286defa7589
Adrian <Adrian@swuser-129-8-201-234.swlan.csufresno.edu>
2018-07-26 13:55:07 -0700

    first commit

---
+++ script.js
@@ -0,0 +1,15 @@
+
+
+  // Initialize Firebase
+  var config = {
+    apiKey: "AIzaSyAVwxF_7YzK9ufnrsCXm3yUv2GyH5jey3E",
+    authDomain: "saige-photo-f8fa5.firebaseapp.com",
+    databaseURL: "https://saige-photo-f8fa5.firebaseio.com",
+    projectId: "saige-photo-f8fa5",
+    storageBucket: "saige-photo-f8fa5.appspot.com",
+    messagingSenderId: "897695085028"
+  };
+  firebase.initializeApp(config);
+
+
+  
